878abd376129bedeab71d040878a1dc2e343ec37
unknown <89057650773a@gmail.com>
2025-03-13 03:06:25 +0300

    Добавленна настройка интервала

--- clicker.py
+++ clicker.py
@@ -3,17 +3,33 @@ import keyboard
 import time
 
 is_clicking = False
+interval = 0.1  
 
 def toggle_clicker():
     global is_clicking
     is_clicking = not is_clicking
     print("Автокликер:", "включен" if is_clicking else "выключен")
 
-keyboard.add_hotkey('ctrl + c', toggle_clicker)
+def increase_interval():
+    global interval
+    interval += 0.1
+    print("Интервал:", interval)
 
-print("Автокликер запущен. Нажмите Ctrl + C для старта/остановки.")
+def decrease_interval():
+    global interval
+    if interval > 0.1:
+        interval -= 0.1
+        print("Интервал:", interval)
+
+keyboard.add_hotkey('ctrl + c', toggle_clicker)
+keyboard.add_hotkey('ctrl + up', increase_interval)
+keyboard.add_hotkey('ctrl + down', decrease_interval)
+
+print("Автокликер запущен. Управление:")
+print("- Ctrl + C: старт/стоп")
+print("- Ctrl + ↑/↓: изменить интервал")
 while True:
     if is_clicking:
-        pyautogui.click() 
-        time.sleep(0.1)    
+        pyautogui.click()
+        time.sleep(interval)
     time.sleep(0.01)
